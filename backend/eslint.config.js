df2a80b63014ab1dab683722cab095511a49e1c0
Ashwani Kumar <itsashu268@gmail.com>
2025-07-24 11:36:11 +0530

    (feature):Added user schema

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,8 +1,11 @@
 // eslint.config.js
 import eslintPluginImport from 'eslint-plugin-import';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 import eslintPluginTs from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+import importPlugin from 'eslint-plugin-import';
 
 export default [
     {
@@ -21,6 +24,9 @@ export default [
         plugins: {
             '@typescript-eslint': eslintPluginTs,
             import: eslintPluginImport,
+            '@typescript-eslint': tsPlugin,
+            import: importPlugin,
+            jsdoc: jsdocPlugin,
         },
         rules: {
             'no-console': 'warn',
@@ -36,6 +42,12 @@ export default [
                 },
             ],
             '@typescript-eslint/explicit-module-boundary-types': 'off',
+            'jsdoc/check-alignment': 'warn',
+            'jsdoc/check-param-names': 'warn',
+            'jsdoc/check-tag-names': 'warn',
+            'jsdoc/check-types': 'warn',
+            'jsdoc/require-param': 'warn',
+            'jsdoc/require-returns': 'warn',
         },
     },
     eslintConfigPrettier,
---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose, {Document, Schema} from "mongoose";
+
+export interface IUser extends Document{
+    username : string;
+    email : string;
+    password : string;
+}
+
+const UserSchema = new Schema<IUser>({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true},
+},
+{
+    timestamps: true
+}
+);
+
+
+export const UserModel = mongoose.model<IUser>('User', UserSchema)
